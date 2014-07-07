2c6e3bec1e3439b981a4e17fa53ef7ef3cdaade2
Jacob Martin <jacobm@angelvisiontech.com>
2014-07-07 11:54:46 -0700

    Slug functions

--- src/controllers/admin/AdminAngelController.php
+++ src/controllers/admin/AdminAngelController.php
@@ -18,6 +18,53 @@ class AdminAngelController extends AngelController {
 	}
 
 	/**
+	 * Handle the creation of the slug and verifying that it is unique.
+	 * Slugs are used for URLs generally, like: http://yoursite.com/products/large-green-ball
+	 *
+	 * @param string $model - The model name.
+	 * @param string $column - The column name where the slug is stored.
+	 * @param string $value - The value to sluggify (usually the 'name' field from input).
+	 * @param null $id - The ID of the current object if editing.
+	 * @return string $unique_slug - The unique slug.
+	 */
+	public function slug($model, $column, $value, $id = null)
+	{
+		$slug        = $this->sluggify($value);
+		$unique_slug = $slug;
+		$i           = 1;
+
+		do {
+			$counter = $model::where($column, $slug);
+			if ($id) $counter = $counter->where($id, '<>', $id);
+			$counter = $counter->count();
+			if ($counter) {
+				$unique_slug = $slug . '-' . $i++;
+			}
+		} while ($counter);
+
+		return $unique_slug;
+	}
+
+	/**
+	 * Turn a string into a slug.
+	 * i.e.: 'Large Green Ball' -> 'large-green-ball'
+	 *
+	 * @param string $name - The string to sluggify.
+	 * @return string $slug - The sluggified string.
+	 */
+	private function sluggify($name)
+	{
+		$slug = strtolower($name);
+		$slug = strip_tags($slug);
+		$slug = stripslashes($slug);
+
+		$slug = preg_replace('/[^a-z0-9]+/', '-', $slug);
+		$slug = trim($slug, '-');
+
+		return $slug;
+	}
+
+	/**
 	 * Handle adding new menu items when creating content (such as pages) from within the menu system.
 	 *
 	 * @param string $fmodel - Name of the model.
