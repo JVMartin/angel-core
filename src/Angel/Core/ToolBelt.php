c0322df657b49d148bc4b57d8de0c00e7d2aa026
Jacob Martin <jacobm@angelvisiontech.com>
2014-08-04 11:40:18 -0700

    Method stubs

--- src/Angel/Core/ToolBelt.php
+++ src/Angel/Core/ToolBelt.php
@@ -24,6 +24,11 @@ class ToolBelt {
 		return (int)str_replace('.', '', number_format((float)$dollars, 2, '.', ''));
 	}
 
+	/**
+	 * Get the MySQL version of the server.
+	 *
+	 * @return string - i.e.: '5.5.12'
+	 */
 	static function mysql_version()
 	{
 		$version = DB::select('SELECT VERSION() AS `version`');
@@ -32,6 +37,11 @@ class ToolBelt {
 		return $version;
 	}
 
+	/**
+	 * Test if the MySQL version is greater than a given number.
+	 *
+	 * i.e.:  \ToolBelt::mysql_greater(5, 5, 10);  // Greater than 5.5.10?
+	 */
 	static function mysql_greater($a, $b, $c)
 	{
 		$version = explode('.', static::mysql_version());
