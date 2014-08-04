8871bf2500a9ad7021072f201d9cd936191cbb76
Jacob Martin <jacobm@angelvisiontech.com>
2014-08-04 11:30:28 -0700

    MySQL version checkers

--- src/Angel/Core/ToolBelt.php
+++ src/Angel/Core/ToolBelt.php
@@ -24,4 +24,19 @@ class ToolBelt {
 		return (int)str_replace('.', '', number_format((float)$dollars, 2, '.', ''));
 	}
 
+	static function mysql_version()
+	{
+		$version = DB::select('SELECT VERSION() AS `version`');
+		$version = explode('-', $version[0]->version);
+		$version = $version[0];
+		return $version;
+	}
+
+	static function mysql_greater($a, $b, $c)
+	{
+		$version = explode('.', static::mysql_version());
+		if ($a <= $version[0] && $b <= $version[1] && $c <= $version[2]) return true;
+		return false;
+	}
+
 }
