bc4fb2dee5f396993d6b45adf32dbe10cfb164a9
Jacob Martin <jacobm@angelvisiontech.com>
2014-08-26 13:22:28 -0700

    Guest filter and verbage

--- src/filters.php
+++ src/filters.php
@@ -1,12 +1,16 @@
 <?php
 
+Route::filter('guest', function() {
+	if (Auth::check()) return Redirect::to('/')->withErrors('You must be signed out to view that page.');
+});
+
 Route::filter('auth', function () {
 	if (Auth::guest()) return Redirect::guest('signin');
 });
 
 Route::filter('admin', function () {
 	if (Auth::guest()) return Redirect::guest('signin');
-	if (!Auth::user()->is_admin()) return Redirect::guest('signin')->withErrors('You must be logged in as an administrator to view that page.');
+	if (!Auth::user()->is_admin()) return Redirect::guest('signin')->withErrors('You must be signed in as an administrator to view that page.');
 });
 
 Route::filter('nonadmin', function () {
@@ -16,6 +20,6 @@ Route::filter('nonadmin', function () {
 Route::filter('superadmin', function () {
 	if (Auth::guest()) return Redirect::guest('signin');
 	if (!Auth::user()->is_superadmin()) {
-		return Redirect::to(admin_uri('/'))->withErrors('You must be logged in as a super administrator to view that page.');
+		return Redirect::to(admin_uri('/'))->withErrors('You must be signed in as a super administrator to view that page.');
 	}
 });
